3f9b87f0ad40eafbaf81dce610d3b2d56210155f
Vladislav <melnikovv717@gmail.com>
2023-11-24 23:54:00 +0300

    Правки в App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
 import axios from "axios";
